d9d894f9d3f7c6026cb0d04b31cc3baa6d9b0285
thiago henrique da silva <th.ferrari@outlook.com.br>
2022-02-11 21:01:57 -0300

    add name in socket

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 [] - Add username
 [] - Listar usuarios online
+[] - Pode clicar no usuario e mandar msg privada
 [] - Enviar msg privadas
--- src/websockets.js
+++ src/websockets.js
@@ -1,13 +1,20 @@
 const { io } = require('./http')
 
-io.on('connection', (socket) => {
+io.on('connection',  (socket) => {
+    let usersOnline = []
     console.log(`user ID: ${socket.id}`)
     console.log('a user connect')
     socket.on('disconnect', () => console.log('user disconnected'))
-    socket.on('select_room', data => {
+    socket.on('select_room', async data => {
         room = data.room
         socket.join(data.room)
-        io.emit('user online', data.username)
+        socket.data.username = data.username
+        const socketsOnline = await io.in(data.room).fetchSockets()
+        for (const socket of socketsOnline) {  
+            io.emit('user online', socket.data.username)
+            console.log(socket.id);
+            console.log(socket.data);  
+        }
     })
     socket.on('chat message', data => {
         io.to(data.room).emit('chat message', data.msg)// io é pra quando queremos enviar global, nesse caso para todos da room.
